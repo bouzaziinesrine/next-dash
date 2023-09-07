50b8a115f5c2ee56b69c3e32a67878200b83ad8c
bouzaziinesrine <bouzazinesrine10@gmail.com>
2023-09-07 13:16:35 +0100

    fixed

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -31,11 +31,11 @@ const Sidebar = ({children}) => {
                         <HiOutlineShoppingBag size={20}/>
                     </div>
                 </Link>
-                <Link href='/'>
+                {/*<Link href='/'>
                     <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
                         <FiSettings size={20}/>
                     </div>
-                </Link>
+  </Link>*/}
             </div>
 
         </div>
